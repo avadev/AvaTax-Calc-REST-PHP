764d7f192f29631c15cd712555be3238d0dc16ed
Douglas Greenshields <douglas@usemarkup.com>
2015-07-16 11:27:50 +0200

    declare a file for namespaced exception class, and use it

--- src/AvaTax/AddressServiceRest.php
+++ src/AvaTax/AddressServiceRest.php
@@ -21,7 +21,6 @@
  */
 
 namespace AvaTax;
-class Exception extends \Exception {}
 
 class AddressServiceRest 
 {
@@ -50,8 +49,8 @@ class AddressServiceRest
 	public function validate($validateRequest)
 	{
 		if(!(filter_var($this->config['url'],FILTER_VALIDATE_URL)))			throw new \Exception("A valid service URL is required.");
-		if(empty($this->config['account']))		throw new \Exception("Account number or username is required.");
-		if(empty($this->config['license']))		throw new \Exception("License key or password is required.");
+		if(empty($this->config['account']))		throw new Exception("Account number or username is required.");
+		if(empty($this->config['license']))		throw new Exception("License key or password is required.");
 
 		$url =  $this->config['url'].'/1.0/address/validate?'. http_build_query($validateRequest->getAddress());
 		$curl = curl_init();
